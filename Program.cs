929e6f4bff2b64fd4a60c7bd91c7f90f3fa941bd
unknown <brenomax02@gmail.com>
2020-06-22 10:28:12 -0300

    Projeto Finalizado

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace Aula22Static
         {
             Conversor.CotacaoDolar = 5.29f;
             Conversor.CotacaoEuro = 5.88f;
-            Console.WriteLine(Conversor.ConverterRealParaDolar(30f));
-            Console.WriteLine(Conversor.ConverterRealParaDolar(499.99f));
-            Console.WriteLine(Conversor.ConverterRealParaEuro(58f));
-            Console.WriteLine(Conversor.ConverterEuroParaReal(55.50f));
-            Console.WriteLine(Conversor.ConverterDolarParaEuro(28f));
+            Console.WriteLine($"O valor em real é de $ {Conversor.ConverterRealParaDolar(30f)}");
+            Console.WriteLine($"O valor em dolar é de R$ {Conversor.ConverterRealParaDolar(499.99f)}");
+            Console.WriteLine($"O valor em real é de € {Conversor.ConverterRealParaEuro(58f)}");
+            //O operador de euro não é aceito no terminal, então fica "?"
+            Console.WriteLine($"O valor em euro é de $ {Conversor.ConverterEuroParaReal(55.50f)}");
+            Console.WriteLine($"O valor em euro é de $ {Conversor.ConverterDolarParaEuro(28f)}");
         }
     }
 }
